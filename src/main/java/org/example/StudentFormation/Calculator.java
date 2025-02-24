7bef7a760d3ac6aed1745943577f2fd1a0cb4978
Adam El Matni <elmatniadam5@gmail.com>
2025-02-24 12:29:29 +0000

    Adding test

---
+++ src/main/java/org/example/StudentFormation/Calculator.java
@@ -0,0 +1,7 @@
+package org.example.StudentFormation;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.example.StudentFormation.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+}
